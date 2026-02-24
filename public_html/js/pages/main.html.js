b5849f29126db016ddbb170bba50a13c3fb3cd43
Eric Schreffler <omnicrola@gmail.com>
2026-02-23 21:41:29 -0500

    removed images and smartlaunch as distinct sections, will re-add as individual projects

--- public_html/js/pages/main.html.js
+++ public_html/js/pages/main.html.js
@@ -5,23 +5,11 @@ window.module.triggers = (() => {
             transitionInterval: 3000,
             startDelay: 0,
         });
-        Slideshow.create({
-            targetSelector: '#slides-images',
-            transitionInterval: 3000,
-            startDelay: 100,
-        });
-        Slideshow.create({
-            targetSelector: '#slides-smartlaunch',
-            transitionInterval: 3000,
-            startDelay: 300,
-        });
         return Promise.resolve();
     }
 
     function _onUnload() {
         Slideshow.destroy('#slides-projects');
-        Slideshow.destroy('#slides-images');
-        Slideshow.destroy('#slides-smartlaunch');
     }
 
     return {
